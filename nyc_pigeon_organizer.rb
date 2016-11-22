29338f897f579f4fcbf5c7fecfd383ff730d4c60
limerentfox <christi@limerentfox.com>
2016-11-22 16:21:21 -0500

    nyc-pigeon-organizer lab complete

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do |category, info|
+    info.each do |attributes, names|
+      names.each do |pigeon|
+        if pigeon_list[pigeon].nil?
+          pigeon_list[pigeon] = {}
+        end
+        if pigeon_list[pigeon][category].nil?
+          pigeon_list[pigeon][category] = []
+        end
+        pigeon_list[pigeon][category] << attributes.to_s
+      end
+    end
+  end
+  pigeon_list
 end
